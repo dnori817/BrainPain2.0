e022e530f3a21efe612a8e68f8d622d99e772035
Nori <dnori817@gmail.com>
2018-02-20 20:19:21 -0500

    "Work on render Question"

--- components/Question.js
+++ components/Question.js
@@ -7,7 +7,42 @@ export default class Question extends Component {
 
     const { correct_answer, incorrect_answers } = this.props.question;
     const answerList = this.shuffle(incorrect_answers.concat([correct_answer]));
-    
+
+    this.state = {
+      hasAnswered: false,
+      isCorrect: false,
+      answerSelected: '',
+      answerList,
+    };
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (this.props.question !== nextProps.question) {
+      const { correct_answer, incorrect_answers } =
+      nextProps.question;
+      const answerList =
+      this.shuffle(incorrect_answers.concat([correct_answer]));
+
+      this.setState({
+        hasAnswered: false,
+        isCorrect: false,
+        answerSelected: '',
+        answerList,
+      });
+    }
+  }
+
+  render() {
+    const { question } = this.props.question;
+
+    return (
+      <View>
+        <Text>
+
+        </Text>
+      </View>
+    )
+  }
+
+  renderAnswer = (answer)
 }
